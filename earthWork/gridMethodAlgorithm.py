e21843e2192944e0dc6fe967ab969e947a8b75f7
KTingLee <58084608+KTingLee@users.noreply.github.com>
2023-03-10 16:44:56 +0800

    refactor(earthwork): 重構將原始高程mapping至 nan高程

--- earthWork/gridMethodAlgorithm.py
+++ earthWork/gridMethodAlgorithm.py
@@ -10,15 +10,16 @@ def findDeltaValue(deltaArray):
 
 path = './sourceData/'
 fileName = 'B區點位.csv'
+
+# 讀取檔案，讀取後的欄位分別是Z,X,Y
 elevationData = np.genfromtxt(f"{path}{fileName}", delimiter=',', skip_header = 1, usecols = (2, 3, 4))
-# elevationData = np.loadtxt(path, skiprows=1)
 
 # 排序
 elevationData = elevationData[np.lexsort(
     (elevationData[:, 1], elevationData[:, 2])
 )]  # 先依y排，再依x排
 
-# diff找delta
+# diff找delta(網格長寬)
 delta = np.diff(elevationData[:, [1, 2]], axis=0)  # axis=0: 依row做diff
 deltaX = findDeltaValue(delta[:, 0])
 deltaY = findDeltaValue(delta[:, 1])
@@ -38,13 +39,13 @@ elevationArr = np.empty((lengthY, lengthX,)) * np.nan
 for [z, x, y] in elevationData:
     col = (x - minX) / deltaX
     row = (maxY - y) / deltaY  # 因為y是由下往上增加，所以計算index的方式要顛倒
-    if ((col % 1 > 0.0) | (row % 1 > 0.0)):  # 過濾奇點
-        continue
-    elevationArr[int(row)][int(col)] = z
+    if col.is_integer() and row.is_integer():  # 過濾奇點
+        elevationArr[int(row)][int(col)] = z
 
 result = []
-for row in range(elevationArr.shape[0] - 1):
-    for col in range(elevationArr.shape[1] - 1):
+rows, cols = elevationArr.shape
+for row in range(rows - 1):
+    for col in range(cols - 1):
         a = elevationArr[row][col]
         b = elevationArr[row][col + 1]
         c = elevationArr[row + 1][col]
