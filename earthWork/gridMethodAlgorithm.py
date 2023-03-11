bae1d0929d58d49031414f5848424e87e22fac2f
KTingLee <58084608+KTingLee@users.noreply.github.com>
2023-03-11 16:20:39 +0800

    refactor(earthwork): 重構程式碼，主要是把內容抽成函數 1). 將 xCoords, yCoords, zCoords 從 elevationArr 抽出，讓程式碼更容易閱讀
    補充:
    1). int((gridInfo['maxX'] - gridInfo['minX']) / gridInfo['deltaX'] + 1)，原本是想去掉 int 並利用 // 處理，但因為值的型態為 numpy.float64 所以沒辦法

--- earthWork/gridMethodAlgorithm.py
+++ earthWork/gridMethodAlgorithm.py
@@ -2,11 +2,62 @@ import numpy as np
 from scipy import stats
 # from matplotlib import pyplot as plt
 
+# diff找delta(網格長寬)
+def findDeltaValue(axisCoords):
+    delta = np.diff(axisCoords)
+    delta = delta[delta > 0]  # 去除0
+    return stats.mode(delta, keepdims=True)[0][0]  # 找眾數
 
-def findDeltaValue(deltaArray):
-    deltaArray = deltaArray[deltaArray > 0]  # 去除0
-    return stats.mode(deltaArray, keepdims=True)[0][0]  # 找眾數
+def getMaxMinValue(array):
+    maxValue = np.amax(array)
+    minValue = np.amin(array)
+    return maxValue, minValue
 
+# 取得最大最小值，並計算出網格長度
+def getGridInfo(xCoords, yCoords):
+    maxX, minX = getMaxMinValue(xCoords)
+    maxY, minY = getMaxMinValue(yCoords)
+    return {
+        'deltaX': findDeltaValue(xCoords),
+        'deltaY': findDeltaValue(yCoords),
+        'maxX': maxX,
+        'minX': minX,
+        'maxY': maxY,
+        'minY': minY
+    }
+
+# 初始化完整邊界陣列(利用xy座標算出網格大小後，產生對應範圍的nan陣列)
+def genNaNElevationArr(gridInfo):
+    # 計算邊界元素數量
+    colSize = int((gridInfo['maxX'] - gridInfo['minX']) / gridInfo['deltaX'] + 1)
+    rowSize = int((gridInfo['maxY'] - gridInfo['minY']) / gridInfo['deltaY'] + 1)
+    return np.empty((rowSize, colSize,)) * np.nan
+
+# 將原始高程資料依照其座標放置到nan array中對應位置
+def mappingElevationToNaNArr(elevationData, nanElevationArr, gridInfo):
+    # 將原始資料由 x, y 轉換成 indexX, indexY
+    for [z, x, y] in elevationData:
+        col = (x - gridInfo['minX']) / gridInfo['deltaX']
+        row = (gridInfo['maxY'] - y) / gridInfo['deltaY']  # 因為y是由下往上增加，所以計算index的方式要顛倒
+        if col.is_integer() and row.is_integer():  # 過濾奇點
+            nanElevationArr[int(row)][int(col)] = z
+    return nanElevationArr
+
+# 將方格高程資料轉換成四點法資料
+def createGridRowsData(gridElevationArr):
+    result = []
+    rows, cols = gridElevationArr.shape
+    for row in range(rows - 1):
+        for col in range(cols - 1):
+            a = gridElevationArr[row][col]
+            b = gridElevationArr[row][col + 1]
+            c = gridElevationArr[row + 1][col]
+            d = gridElevationArr[row + 1][col + 1]
+            elevations = [a, b, c, d]
+            if np.isnan(elevations).any():
+                continue
+            result.append(elevations)
+    return result
 
 path = './sourceData/'
 fileName = 'B區點位.csv'
@@ -19,40 +70,11 @@ elevationData = elevationData[np.lexsort(
     (elevationData[:, 1], elevationData[:, 2])
 )]  # 先依y排，再依x排
 
-# diff找delta(網格長寬)
-delta = np.diff(elevationData[:, [1, 2]], axis=0)  # axis=0: 依row做diff
-deltaX = findDeltaValue(delta[:, 0])
-deltaY = findDeltaValue(delta[:, 1])
+zCoords, xCoords, yCoords = elevationData.T
+gridInfo = getGridInfo(xCoords, yCoords)
+nanElevationArr = genNaNElevationArr(gridInfo)
 
-# 找 max, min 做邊界
-maxZ, maxX, maxY = np.amax(elevationData, axis=0)  # axis=0: 依column找
-minZ, minX, minY = np.amin(elevationData, axis=0)
+elevationArr = mappingElevationToNaNArr(elevationData, nanElevationArr, gridInfo)
+gridElevationRows = createGridRowsData(elevationArr)
 
-# 計算邊界元素數量
-lengthX = int((maxX - minX) / deltaX + 1)
-lengthY = int((maxY - minY) / deltaY + 1)
-
-# 初始化完整邊界陣列
-elevationArr = np.empty((lengthY, lengthX,)) * np.nan
-
-# 將原始資料由 x, y 轉換成 indexX, indexY
-for [z, x, y] in elevationData:
-    col = (x - minX) / deltaX
-    row = (maxY - y) / deltaY  # 因為y是由下往上增加，所以計算index的方式要顛倒
-    if col.is_integer() and row.is_integer():  # 過濾奇點
-        elevationArr[int(row)][int(col)] = z
-
-result = []
-rows, cols = elevationArr.shape
-for row in range(rows - 1):
-    for col in range(cols - 1):
-        a = elevationArr[row][col]
-        b = elevationArr[row][col + 1]
-        c = elevationArr[row + 1][col]
-        d = elevationArr[row + 1][col + 1]
-        elevations = [a, b, c, d]
-        if np.isnan(elevations).any():
-            continue
-        result.append(elevations)
-
-np.savetxt(fileName, result, delimiter=',', fmt='%.3f')
+np.savetxt('test.csv', gridElevationRows, delimiter=',', fmt='%.3f')
